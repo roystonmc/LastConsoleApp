63c8dff82f0bcf7864d1077ef20272875b9407a3
rmcneill <rmcneill@gmail.com>
2025-01-21 19:52:11 -0500

    working version of code

---
+++ LightsPlugin.cs
@@ -0,0 +1,50 @@
+using System.ComponentModel;
+using System.Text.Json.Serialization;
+using Microsoft.SemanticKernel;
+
+public class LightsPlugin
+{
+   // Mock data for the lights
+   private readonly List<LightModel> lights = new()
+   {
+      new LightModel { Id = 1, Name = "Table Lamp", IsOn = false },
+      new LightModel { Id = 2, Name = "Porch light", IsOn = false },
+      new LightModel { Id = 3, Name = "Chandelier", IsOn = true }
+   };
+
+   [KernelFunction("get_lights")]
+   [Description("Gets a list of lights and their current state")]
+   public async Task<List<LightModel>> GetLightsAsync()
+   {
+      return lights;
+   }
+
+   [KernelFunction("change_state")]
+   [Description("Changes the state of the light")]
+   public async Task<LightModel?> ChangeStateAsync(int id, bool isOn)
+   {
+      var light = lights.FirstOrDefault(light => light.Id == id);
+
+      if (light == null)
+      {
+         return null;
+      }
+
+      // Update the light with the new state
+      light.IsOn = isOn;
+
+      return light;
+   }
+}
+
+public class LightModel
+{
+   [JsonPropertyName("id")]
+   public int Id { get; set; }
+
+   [JsonPropertyName("name")]
+   public string Name { get; set; }
+
+   [JsonPropertyName("is_on")]
+   public bool? IsOn { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Import packages
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+// Populate values from your OpenAI deployment
+var modelId = "test-gpt-35-turbo-deployment";
+var endpoint = "https://rmc-ai-dev.openai.azure.com/";
+var apiKey = "de484c3fe6e24e9e83a468231ced2a15";
+
+// Create a kernel with Azure OpenAI chat completion
+var builder = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(modelId, endpoint, apiKey);
+
+// Add enterprise components
+builder.Services.AddLogging(services => services.AddConsole().SetMinimumLevel(LogLevel.Trace));
+
+// Build the kernel
+Kernel kernel = builder.Build();
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+// Add a plugin (the LightsPlugin class is defined below)
+kernel.Plugins.AddFromType<LightsPlugin>("Lights");
+
+// Enable planning
+OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new() 
+{
+    FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+};
+
+// Create a history store the conversation
+var history = new ChatHistory();
+
+// Initiate a back-and-forth chat
+string? userInput;
+do {
+    // Collect user input
+    Console.Write("User > ");
+    userInput = Console.ReadLine();
+
+    // Add user input
+    history.AddUserMessage(userInput);
+
+    // Get the response from the AI
+    var result = await chatCompletionService.GetChatMessageContentAsync(
+        history,
+        executionSettings: openAIPromptExecutionSettings,
+        kernel: kernel);
+
+    // Print the results
+    Console.WriteLine("Assistant > " + result);
+
+    // Add the message from the agent to the chat history
+    history.AddMessage(result.Role, result.Content ?? string.Empty);
+} while (userInput is not null);
